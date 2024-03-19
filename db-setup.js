6f19358a4404a9e874d6fb98e1896aa80b9164f9
Yamakawa <gomasiopan@gmail.com>
2024-03-19 23:38:16 +0900

    フロントとチェックアウト処理の一部を作成

---
+++ db-setup.js
@@ -0,0 +1,61 @@
+const mysql = require('mysql2');
+// MySQL接続情報
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '20240222'
+});
+function CreateDB() {
+
+    // MySQL接続
+    connection.connect(err => {
+        if (err) {
+            console.error('MySQL接続エラー:', err);
+            throw err;
+        }
+        console.log('MySQLに接続しました');
+    });
+
+    var sql = 'CREATE DATABASE IF NOT EXISTS exam_ec DEFAULT CHARACTER SET utf8;';
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.error('データベース作成エラー:', err);
+            throw err;
+        }
+        console.log('データベースが作成されました');
+    });
+
+    // テーブル作成
+    sql = 'CREATE TABLE IF NOT EXISTS exam_ec.customer' +
+        ' (`username` mediumtext, `email` mediumtext, `zip` mediumtext,' +
+        ' `address` mediumtext,`passwd` mediumtext) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.error('customerテーブル作成エラー:', err);
+            throw err;
+        }
+        console.log('customerテーブルが作成されました');
+    });
+
+    sql = 'CREATE TABLE IF NOT EXISTS exam_ec.products' +
+        ' (`itemcd` int(11) NOT NULL, `itemname` varchar(255) DEFAULT NULL,' +
+        ' `itemprice` decimal(10,2) NOT NULL, `description` mediumtext, `filepath`' +
+        ' mediumtext, PRIMARY KEY (`itemcd`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.error('productsテーブル作成エラー:', err);
+            throw err;
+        }
+        console.log('productsテーブルが作成されました');
+    });
+}
+
+module.exports = {
+    CreateDB: CreateDB
+};
+
+/*
+var dbSetup = require('./db-setup');
+dbSetup.CreateDB();
+ */
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const stripe = require('stripe')('sk_test_51OjLLCCquV7OPeZpqxsUa5CcDkSHaV5B1Ec9uOGcc0EyB2AuvbFim94V5kWljF17zHP42gElTRtZoCxTjhoiegzK009FIzI7Rk');
+const YOUR_DOMAIN = 'http://localhost:3000';
 
 router.get('/', (req, res, next) => {
     res.render('cart', '');
 });
 
+// checkout→カート情報を基にStripeで決済を行う
+router.post('/checkout', async function (req, res) {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'jpy',
+            product_data: {
+              name: '波乗りジョニー',
+            },
+            unit_amount: 1820,
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/',
+      cancel_url: 'http://localhost:3000/cart',
+    });
+    
+  res.redirect(303, session.url);
+});
 module.exports = router;
